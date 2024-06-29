4e2e919ee17f7c998f6f2d34525c14e0bd09a15d
alexandraputanu <72292505+alexandraputanu@users.noreply.github.com>
2024-06-29 13:15:53 +0300

    Summary (required)

---
+++ src/js/01-gallery.js
@@ -0,0 +1,32 @@
+// Descris în documentație
+import SimpleLightbox from 'simplelightbox';
+// Import suplimentar de stil
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+
+function initializeGallery() {
+  const galleryContainer = document.querySelector('.gallery');
+  const galleryMarkup = createGalleryMarkup(galleryItems);
+  galleryContainer.innerHTML = galleryMarkup;
+  initializeLightbox();
+}
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `;
+    })
+    .join('');
+}
+
+function initializeLightbox() {
+  new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+}
+
+initializeGallery();
